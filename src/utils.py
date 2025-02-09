fc4638981edea32d540adb4d5d479b6c38a8728b
Amzad Basha <amzadbasha.shaik@ispace.com>
2025-02-09 12:09:58 +0530

    Updated requirements.txt and util.py cleanup

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,6 @@
 def format_question(question):
     return f"**{question['question']}**"
 
-# def calculate_score(correct_answers, total_questions):
-#     if total_questions == 0:
-#         return 0
-#     return (correct_answers / total_questions) * 100
-
 def calculate_score(questions, user_answers):
     score = 0
     for question, user_answer in zip(questions, user_answers):
